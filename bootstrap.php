8faa2eafcc21a337a9666eb0fe7d7ecee2e848ba
Nguyen Van Thao <thaolaptrinh@gmail.com>
2023-07-06 02:23:30 +0700

    updated

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,10 @@ ini_set('display_errors', 1);
 define('_DIR_ROOT', __DIR__);
 
 $protocol = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
-$base_url = $protocol . '://' . $_SERVER['HTTP_HOST'] . '/';
+$base_url = $protocol . '://' . $_SERVER['HTTP_HOST'];
 $folder = trim(str_replace(($_SERVER['DOCUMENT_ROOT']), '', (__DIR__)), '/');
-$base_url .= !empty($folder) ? $folder : '';
+
+$base_url .= !empty($folder) ? '/' . $folder : '';
 $url = !empty($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
 
 define('VIEW_PATH', __DIR__ . '/app/views/');
